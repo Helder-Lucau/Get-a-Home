df2395e0c4cccfada2365889ca7529376632ceaf
Helder <helderlucau@gmail.com>
2023-07-06 10:55:42 +0300

    fetch data from db.json sucessfully

--- src/main.js
+++ src/main.js
@@ -1,21 +1,54 @@
-const campList = document.getElementById("item-list")
-// document.addEventListener('DOMContentLoaded', () => {
+const apiURL = 'http://localhost:3000'
+const campList = document.querySelector("#camp-profile")
 
+// document.addEventListener('DOMContentLoaded', function () {
+    
+    document.querySelector('form').addEventListener('submit',(e) => {
+        e.preventDefault()
+        fetchCampsData()
+    })
 // })
 
 function fetchCampsData(){
-    fetch('http://localhost:3000/camps')
+    fetch(`${apiURL}/camps`)
     .then((res) => res.json())
     .then((data) => {
         data.forEach(campData => {
-            const card = document.createElement('list')
-            card.className = 'camp-card'
-            card.innerHTML = `
-                <img src="${campData.image}" alt="shelter" class="image-card">
-            `
-            campList.appendChild(card)
+            
+            const campImage = document.createElement('img')
+            campImage.className = 'camp-image'
+            campImage.src = campData.image
+            campList.appendChild(campImage)
+
+            const campName = document.createElement('h3')
+            campName.className = 'camp-name'
+            campName.innerText = campData.campname
+            campList.appendChild(campName)
+
+            const campDesc = document.createElement('p')
+            campDesc.className = 'camp-desc'
+            campDesc.innerText = campData.description
+            campList.appendChild(campDesc)
+
+            const country = document.createElement('p')
+            country.className = 'camp-location'
+            country.innerText = `Country: ${campData.location}`
+            campList.appendChild(country)
+
+            const mail = document.createElement('p')
+            mail.className = 'camp-mail'
+            mail.innerText = `Email Address: ${campData.email}`
+            campList.appendChild(mail)
+
+            const cap = document.createElement('p')
+            cap.className = 'camp-cap'
+            cap.innerText = `: ${campData.people} People`
+            campList.appendChild(cap)
         })
      })
+    .catch(error => {
+        console.log('Error:', error);
+    })
 }
 
 function initialize(){
