eb3d37c63798fe610dc160005d066fc3093ccbac
Helder <helderlucau@gmail.com>
2023-07-06 17:04:12 +0300

    main section styling

--- src/main.js
+++ src/main.js
@@ -2,15 +2,14 @@ const apiURL = 'http://localhost:3000'
 const campList = document.querySelector(".camp-content")
 const campDetails = document.querySelector(".camp-details")
 
-// document.addEventListener('DOMContentLoaded', function () {
-    
+function searchCamps(){
     document.querySelector('form').addEventListener('submit',(e) => {
         e.preventDefault()
         
         //data validation
         
     })
-// })
+} 
 
 function fetchCampsData(){
     fetch(`${apiURL}/camps`)
@@ -20,63 +19,66 @@ function fetchCampsData(){
 
            const camp = document.createElement('div')
            camp.className = 'camp-list'
-           camp.src = campData.image
-           camp.innerHTML= campData.campname
-           campList.append(camp)
+           camp.innerHTML = `
+                <img src="${campData.image}" alt="${campData.image}" class="camp-poster">
+                <h3>${campData.campname}</h3>
+                <p>Country: ${campData.location}</p>
+           `
+           campList.appendChild(camp)
 
            camp.addEventListener('click', () => {
                 campsDataDetails(campData)
            })
         })
+    })
     .catch(error => {
         console.log('Error:', error);
-        })
     })
 }
 
 //Function that fetch camp data from db.json
 function campsDataDetails(campData){
 
-            campDetails.innerHTML = ""
+    campDetails.innerHTML = "";
 
-            const campProfile = document.createElement('div')
-            campProfile.className = 'camp-list'
-            campDetails.appendChild(campProfile)
-            
-            const campImage = document.createElement('img')
-            campImage.className = 'camp-image'
-            campImage.src = campData.image
-            campProfile.append(campImage)
+    const campProfile = document.createElement("div");
+    campProfile.className = "camp-list";
+    campDetails.appendChild(campProfile);
 
-            const campName = document.createElement('h3')
-            campName.className = 'camp-name'
-            campName.innerText = campData.campname
-            campProfile.append(campName)
+    const campImage = document.createElement("img");
+    campImage.className = "camp-image";
+    campImage.src = campData.image;
+    campProfile.append(campImage);
 
-            const campDesc = document.createElement('p')
-            campDesc.className = 'camp-desc'
-            campDesc.innerText = campData.description
-            campProfile.appendChild(campDesc)
+    const campName = document.createElement("h3");
+    campName.className = "camp-name";
+    campName.innerText = campData.campname;
+    campProfile.append(campName);
 
-            const country = document.createElement('p')
-            country.className = 'camp-location'
-            country.innerText = `Country: ${campData.location}`
-            campProfile.appendChild(country)
+    const campDesc = document.createElement("p");
+    campDesc.className = "camp-desc";
+    campDesc.innerText = campData.description;
+    campProfile.appendChild(campDesc);
 
-            const mail = document.createElement('p')
-            mail.className = 'camp-mail'
-            mail.innerText = `Email Address: ${campData.email}`
-            campProfile.appendChild(mail)
+    const country = document.createElement("p");
+    country.className = "camp-location";
+    country.innerText = `Country: ${campData.location}`;
+    campProfile.appendChild(country);
 
-            const cap = document.createElement('p')
-            cap.className = 'camp-cap'
-            cap.innerText = `Capacity: ${campData.people} People`
-            campProfile.appendChild(cap)
+    const mail = document.createElement("p");
+    mail.className = "camp-mail";
+    mail.innerText = `Email Address: ${campData.email}`;
+    campProfile.appendChild(mail);
 
-            const volunteerBtn = document.createElement('button')
-            volunteerBtn.className = 'volunteer-btn'
-            volunteerBtn.innerText = 'Volunteer'
-            campProfile.append(volunteerBtn)
+    const cap = document.createElement("p");
+    cap.className = "camp-cap";
+    cap.innerText = `Capacity: ${campData.people} People`;
+    campProfile.appendChild(cap);
+
+    const volunteerBtn = document.createElement("button");
+    volunteerBtn.className = "volunteer-btn";
+    volunteerBtn.innerText = "Volunteer";
+    campProfile.append(volunteerBtn);
    
 }
 
