8313b825f348d6a2b3e645a4e510c7e055ec4184
Helder <helderlucau@gmail.com>
2023-07-06 12:39:28 +0300

    appending elements in a list

--- src/main.js
+++ src/main.js
@@ -25,31 +25,33 @@ function fetchCampsData(){
             const campName = document.createElement('h3')
             campName.className = 'camp-name'
             campName.innerText = campData.campname
-            campList.append(campName)
+            campList.appendChild(campName)
 
             const campDesc = document.createElement('p')
             campDesc.className = 'camp-desc'
             campDesc.innerText = campData.description
-            campList.append(campDesc)
+            campList.appendChild(campDesc)
 
             const country = document.createElement('p')
             country.className = 'camp-location'
             country.innerText = `Country: ${campData.location}`
-            campList.append(country)
+            campList.appendChild(country)
 
             const mail = document.createElement('p')
             mail.className = 'camp-mail'
             mail.innerText = `Email Address: ${campData.email}`
-            campList.append(mail)
+            campList.appendChild(mail)
 
             const cap = document.createElement('p')
             cap.className = 'camp-cap'
             cap.innerText = `Capacity: ${campData.people} People`
-            campList.append(cap)
+            campList.appendChild(cap)
 
             const donateBtn = document.createElement('button')
+            donateBtn.className = 'donote-btn'
             donateBtn.innerText = 'donate'
-            
+            campList.appendChild(donateBtn)
+
 
         })
      })
