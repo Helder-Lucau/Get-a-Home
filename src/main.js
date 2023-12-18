e9aa3cc294b22735388ca357e7251873a3093021
Helder-Lucau <helderlucau@gmail.com>
2023-12-18 09:03:26 +0100

    navbar responsive to mobile

--- src/main.js
+++ src/main.js
@@ -159,9 +159,16 @@ function toggleSidebarDropdown() {
 }
 
 //Close sidebar Menu
-document.getElementById('closeMenu').addEventListener('click', function () {
-    document.getElementById('closeNav').classList.toggle('hidden')
-})
+function Menu(e) {
+    let list = document.querySelector('ul');
+    e.name === 'menu'
+        ? ((e.name = 'close'),
+            list.classList.add('top-[80px]'),
+            list.classList.add('opacity-100'))
+        : ((e.name = 'menu'),
+            list.classList.remove('top-[80px]'),
+            list.classList.remove('opacity-100'));
+}
 
 function initialize() {
 
