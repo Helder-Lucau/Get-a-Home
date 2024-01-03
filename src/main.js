1bc673fe2caa48e2f99a2d0f818e2fa611e0abb5
Helder-Lucau <helderlucau@gmail.com>
2024-01-03 17:55:07 +0100

    fetch data from camps.json

--- src/main.js
+++ src/main.js
@@ -1,101 +1,100 @@
-// // const apiURL = 'https://main-json.onrender.com'
-// const campList = document.querySelector(".camp-content")
-// const campDetails = document.querySelector(".camp-details")
-// const searchInput = document.getElementById('search-form')
-// const searchResult = document.getElementById('search-result')
-// const searchBtn = document.querySelector('#search')
+const campList = document.querySelector(".camp-content")
+const campDetails = document.querySelector(".camp-details")
 
-// //Modal box pop up
-// //Displays a input form to enter donation details
-// // document.querySelector('.donate-btn').addEventListener('click', function () {
-// //     document.querySelector('.bg-modal').style.display = 'flex'
-// // })
+//Modal box pop up
+//Displays a input form to enter donation details
+// document.querySelector('.donate-btn').addEventListener('click', function () {
+//     document.querySelector('.bg-modal').style.display = 'flex'
+// })
 
-// // document.querySelector('.close').addEventListener('click', function () {
-// //     document.querySelector('.bg-modal').style.display = 'none'
-// // })
+// document.querySelector('.close').addEventListener('click', function () {
+//     document.querySelector('.bg-modal').style.display = 'none'
+// })
 
-// function fetchCampsData() {
-//     fetch(`${apiURL}/camps`)
-//         .then((res) => res.json())
-//         .then((data) => {
-//             data.forEach((campData) => {
+function fetchCampsData() {
+    fetch('camps.json')
+        .then((res) => {
+            return res.json()
+        })
+        .then((data) => {
+            console.log(data);
+            data.forEach((campData) => {
 
-//                 const camp = document.createElement('div')
-//                 camp.className = 'camp-list'
-//                 camp.innerHTML = `
-//                 <img src="${campData.image}" alt="${campData.image}" class="camp-poster">
-//                 <h3>${campData.campname}</h3>
-//                 <p><span>Country:</span> ${campData.location}</p>
-//            `
-//                 const readMore = document.createElement('button')
-//                 readMore.className = 'read-more'
-//                 readMore.innerText = 'READ MORE'
+                const camp = document.createElement('div')
+                camp.className = 'camp-list'
+                camp.innerHTML = `
+                    <img src="${campData.image}" alt="${campData.image}" class="camp-poster">
+                    <h3>${campData.campname}</h3>
+                    <p><span>Country:</span> ${campData.location}</p>
+               `
+                const readMore = document.createElement('button')
+                readMore.className = 'read-more'
+                readMore.innerText = 'READ MORE'
 
-//                 camp.appendChild(readMore)
-//                 campList.appendChild(camp)
+                camp.appendChild(readMore)
+                campList.appendChild(camp)
 
-//                 readMore.addEventListener('click', (e) => {
-//                     e.preventDefault()
-//                     campsDataDetails(campData)
-//                 })
-//             })
-//         })
-//         .catch(error => {
-//             console.log('Error:', error);
-//         })
-// }
+                readMore.addEventListener('click', (e) => {
+                    e.preventDefault()
+                    campsDataDetails(campData)
+                })
+            })
+        })
+        .catch(error => {
+            console.log('Error:', error);
+        })
+}
 
-// //Function that fetch camp data from db.json
-// function campsDataDetails(campData) {
+//Function that fetch camp data from db.json
+function campsDataDetails(campData) {
 
-//     campDetails.innerHTML = "";
+    campDetails.innerHTML = "";
 
-//     const campProfile = document.createElement("div");
-//     campProfile.className = "camp-list";
-//     campDetails.appendChild(campProfile);
+    const campProfile = document.createElement("div");
+    campProfile.className = "camp-list";
+    campDetails.appendChild(campProfile);
 
-//     const campImage = document.createElement("img");
-//     campImage.className = "camp-image";
-//     campImage.src = campData.image;
-//     campProfile.append(campImage);
+    const campImage = document.createElement("img");
+    campImage.className = "camp-image";
+    campImage.src = campData.image;
+    campProfile.append(campImage);
 
-//     const campName = document.createElement("h3");
-//     campName.className = "camp-name";
-//     campName.innerText = campData.campname;
-//     campProfile.append(campName);
+    const campName = document.createElement("h3");
+    campName.className = "camp-name";
+    campName.innerText = campData.campname;
+    campProfile.append(campName);
 
-//     const campDesc = document.createElement("p");
-//     campDesc.className = "camp-desc";
-//     campDesc.innerText = campData.description;
-//     campProfile.appendChild(campDesc);
+    const campDesc = document.createElement("p");
+    campDesc.className = "camp-desc";
+    campDesc.innerText = campData.description;
+    campProfile.appendChild(campDesc);
 
-//     const country = document.createElement("p");
-//     country.className = "camp-location";
-//     country.innerText = `Country: ${campData.location}`;
-//     campProfile.appendChild(country);
+    const country = document.createElement("p");
+    country.className = "camp-location";
+    country.innerText = `Country: ${campData.location}`;
+    campProfile.appendChild(country);
 
-//     const cap = document.createElement("p");
-//     cap.className = "camp-cap";
-//     cap.innerText = `Capacity: ${campData.people} People`;
-//     campProfile.appendChild(cap);
-// }
+    const cap = document.createElement("p");
+    cap.className = "camp-cap";
+    cap.innerText = `Capacity: ${campData.people} People`;
+    campProfile.appendChild(cap);
+}
 
-// //Open & Close sidebar Menu
-// function Menu(event) {
-//     let icon = document.querySelector('ul');
-//     event.name === 'menu'
-//         ? ((event.name = 'close'),
-//             icon.classList.add('top-[80px]'),
-//             icon.classList.add('opacity-100'))
-//         : ((event.name = 'menu'),
-//             icon.classList.remove('top-[80px]'),
-//             icon.classList.remove('opacity-100'));
-// }
+//Open & Close sidebar Menu
+function Menu(event) {
+    let icon = document.querySelector('ul');
+    event.name === 'menu'
+        ? ((event.name = 'close'),
+            icon.classList.add('top-[80px]'),
+            icon.classList.add('opacity-100'))
+        : ((event.name = 'menu'),
+            icon.classList.remove('top-[80px]'),
+            icon.classList.remove('opacity-100'));
+}
 
-// function initialize() {
+function initialize() {
 
-//     // fetchCampsData()
+    fetchCampsData()
 
-// }
-// initialize()
+}
+initialize()
